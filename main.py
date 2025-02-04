3735b0d3a90b7c3de39b903809d0ce3e16c1453b
Dread2005 <tshifhiwachedzafordjr@gmail.com>
2025-02-04 20:41:54 +0200

    SMTP added, fixed "I" in html paragraph, fixed "send me a message"

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template, url_for
+from sqlite3 import connect
+
+from flask import Flask, render_template, url_for, request
+import smtplib
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+gapp_pswrd = 'yflw jeuh ryxp vvpq'
+my_email = 'tshifhiwachedzafordjr@gmail.com'
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('website.html')
+    if request.method == 'POST':
+        msg = {
+            'Name': request.form['name'],
+            'Email': request.form['email'],
+            'Msg': request.form['message']
+        }
+
+        print(msg)
+        with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=gapp_pswrd)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=my_email,
+                                msg=f"Subject: A message from a may be employee \n\n"
+                                    f"{msg['Name']}\n"
+                                    f"{msg['Email']}\n"
+                                    f"{msg['Msg']}")
+        return render_template('website.html', methods=['POST'])
+    else:
+        return render_template('website.html', methods=['POST'])
+
 
 if __name__ == '__main__':
      app.run(debug=True)
